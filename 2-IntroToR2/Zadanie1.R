f1bccf96fc5f47a6fcfd6febd423b297954e5c84
Weronika Klecel <109280330+wer-kle@users.noreply.github.com>
2024-10-15 10:57:15 +0200

    Create Zadanie1.R

---
+++ 2-IntroToR2/Zadanie1.R
@@ -0,0 +1,16 @@
+
+  
+  #----------------FUNKCJE POSZCZEGÓLNYCH PANELI----------- 
+
+  # Source (Edytor skryptów) służy do pisania skryptów, a funkcje nie są automatycznie wykonywane
+  # Console (Konsola) służy do...
+
+
+#-------------motyw kolorystyczny------------
+
+# wybrałam motyw 
+  6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
